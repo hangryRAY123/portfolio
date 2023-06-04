9d114c8040c20892cba44a56c9300f3bfc49cbcd
Павел <pasha.seregin.92@mail.ru>
2023-06-04 19:20:43 +0300

    ref

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 document.querySelectorAll('.slider').forEach((n, i) => {
-	window[`slider${i+1}`] = new Swiper(n, {
-		freeMode: true,
-		centeredSlides: true,
-		direction: 'vertical',
-		mousewheel: true,
-		slidesPerView: 1,
-		slidesOffsetBefore: -250
-	})
-})
-bindSwipers(slider1, slider2)
+  window[`slider${i + 1}`] = new Swiper(n, {
+    freeMode: true,
+    centeredSlides: true,
+    direction: 'vertical',
+    mousewheel: true,
+    slidesPerView: 1,
+  });
+});
+bindSwipers(slider1, slider2);
