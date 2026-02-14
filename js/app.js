79f207f431d259e0cc3e39211d4a629bad7bc4f9
hangryRAY123 <89445604+hangryRAY123@users.noreply.github.com>
2026-02-14 20:50:28 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-document.querySelectorAll('.slider').forEach((n, i) => {
+document.querySelectorAll(".slider").forEach((n, i) => {
   window[`slider${i + 1}`] = new Swiper(n, {
-    freeMode: true,
+    freeMode: {
+      enabled: true,
+      momentumBounce: true,
+      momentumBounceRatio: 1,
+    },
+    spaceBetween: 100,   
     // centeredSlides: true,
-    direction: 'vertical',
-    mousewheel: true,
+    direction: "vertical",
+    mousewheel: {
+      sensitivity: 10, // увеличиваем скорость прокрутки
+    },
     slidesPerView: 2.5,
+    // resistanceRatio: 0.5, 
   });
 });
 bindSwipers(slider1, slider2);
